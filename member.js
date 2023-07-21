edfbd83ecee454faf859da82ba45ca330341b0fc
virginia t <140030005+virgtubio@users.noreply.github.com>
2023-07-21 03:59:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsmember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var projects = document.getElementById("projects");
